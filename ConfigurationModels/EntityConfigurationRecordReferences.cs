065d8065a3ee600b6c29431adfaaa2a75f8f0463
fbnhyn <fbnhyn@gmail.com>
2023-03-23 20:00:55 +0100

    chore: generalize naming

---
+++ ConfigurationModels/EntityConfigurationRecordReferences.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration
+{
+    public static class EntityConfigurationRecordReferences
+   
+    {
+        public static readonly Guid ConcreteConfigurationReference = new Guid();
+    }
+}
