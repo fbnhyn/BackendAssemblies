4056718ff064354a6d271f804c3c3720650bab9c
fbnhyn <fbnhyn@gmail.com>
2023-03-23 19:49:43 +0100

    chore: implement a dummy codeactivity for the entity account

---
+++ CodeActivityAssembly/AnAccountCodeActivity.cs
@@ -0,0 +1,24 @@
+﻿using EntityModels;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeActivityAssembly
+{
+    public class AnAccountCodeActivity : CodeActivity
+    {
+        [Output("AccountReference")]
+        [ReferenceTarget(Account.EntityLogicalName)]
+        public OutArgument<EntityReference> AccountReference { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            IWorkflowContext workflowContext = context.GetExtension<IWorkflowContext>();
+            AccountReference.Set(context, new EntityReference(workflowContext.PrimaryEntityName, workflowContext.PrimaryEntityId));
+        }
+    }
+}
